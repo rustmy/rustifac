a4c2671ac6356dec02333b364c529c770e2db640
柯禕藍 <yhilan.ko@gmail.com>
2016-11-07 22:45:54 +1100

    cleanup

--- plugins/ClearSkies.cs
+++ plugins/ClearSkies.cs
@@ -1,21 +1,17 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Reflection;
-namespace Oxide.Plugins
-{
-	[Info("ClearSkies", "Yi", "1.2")]
-	class ClearSkies : RustPlugin
-	{
+namespace Oxide.Plugins {
+	[Info("ClearSkies", "Yi", "1.3")]
+	class ClearSkies : RustPlugin {
 		public class StopDecayReset : RustPlugin {
-	        private void Loaded() {
-	            ConsoleSystem.Run.Server.Normal($"heli.lifetimeminutes 0");
-	        }
-	    }
-		void OnEntitySpawned(BaseNetworkable entity)
-		{
+			private void Loaded() {
+			ConsoleSystem.Run.Server.Normal($"heli.lifetimeminutes 0");
+			}
+		}
+		void OnEntitySpawned(BaseNetworkable entity) {
 			string plane = "cargo_plane";
 			string nade = "grenade.smoke.deployed";
 			string item = entity.name;
-
 			if (item.Contains(plane) || item.Contains(nade)) {
 				Puts($"{entity.name}");
 				entity.Kill();
